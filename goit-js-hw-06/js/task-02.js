9b0853b9b6f837f9b843b58bdf243c7d0af93588
JulitaDworak <julita.dworak@gmail.com>
2023-03-19 12:39:22 +0100

    HW06

--- goit-js-hw-06/js/task-02.js
+++ goit-js-hw-06/js/task-02.js
@@ -7,4 +7,22 @@ const ingredients = [
   'Condiments',
 ];
 
-document.createElement('li')
+const ulList = document.querySelector('ul')
+
+// for( let i = 0; i < ingredients.length; i++) {
+//   const liList = document.createElement('li')
+//   liList.textContent = ingredients[i]
+//   ulList.appendChild(liList)
+// }
+// console.log(ingredients.length);
+// console.log(ulList);
+
+
+const listOfProducts = ingredients
+.map((ingredient) => `<li class="item"> ${ingredient}</li>`)
+// .join="";
+// ulList.insertAdjacentHTML("afterbegin", listOfProducts)
+ulList.innerHTML = listOfProducts
+
+
+console.log(listOfProducts);

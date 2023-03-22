ece8d45a40223e2c67dc839bd72e466b1dcb640a
JulitaDworak <julita.dworak@gmail.com>
2023-03-22 23:46:54 +0100

    HW-06-JS

--- goit-js-hw-06/js/task-10.js
+++ goit-js-hw-06/js/task-10.js
@@ -1,5 +1,46 @@
+const numberOfEl = document.querySelector('input')
+
+
+const createBtn = document.querySelector('[data-create]')
+const destroyBtn = document.querySelector('[data-destroy]')
+const box = document.querySelector('#boxes')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+let sticky = ""
+
+
+const createBoxes = (amount) => {
+  amount = document.querySelector('input[type="number]')
+  sticky = document.createElement('div');
+  box.append(sticky);
+  sticky.style.width = "30px" 
+  sticky.style.height = "30px"
+  sticky.style.backgroundColor = getRandomHexColor()
+  sticky.style.marginTop = "20px"
+  }
+
+const destroyBoxes = () => {
+  box.innerHTML = null
+}
+
+
+// const newOne = () => {
+//   const newOne = document.createElement('div');
+//   box.append(newOne);
+//   newOne.textContent = 'sdfghjkl'
+//   newOne.style.width = "100px"
+//   newOne.style.height = "100px"
+//   newOne.style.color = "blue"
+//   newOne.style.backgroundColor = "blue"
+//   newOne.style.marginTop = "50px"
+//   }
+
+
+
+createBtn.addEventListener('click', createBoxes );
+destroyBtn.addEventListener('click', destroyBoxes )
